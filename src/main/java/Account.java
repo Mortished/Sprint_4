9fba8f60a591a077e593610981c5e2c19f809d2b
s.kukhtanov <s.kukhtanov@tinkoff.ru>
2022-04-02 19:53:41 +0300

    Sprint_4 Task

---
+++ src/main/java/Account.java
@@ -0,0 +1,35 @@
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    /*
+         Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+         Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+    */
+    public boolean checkNameToEmboss() {
+        boolean isCorrect;
+        if (name.length() >= 3 && name.length() <= 19) {
+            isCorrect = true;
+            char spase = ' ';
+            char[] array = name.toCharArray();
+            int spaceCount = 0;
+            for (char a: array) {
+                if (a == spase) spaceCount++;
+            }
+            if (spaceCount != 1) {
+                isCorrect = false;
+            }
+            if (array[0] == spase || array[array.length-1] == spase) {
+                isCorrect = false;
+            }
+        } else {
+            isCorrect = false;
+        }
+        return isCorrect;
+    }
+
+}
---
+++ src/test/java/AccountCheckNameTests.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class AccountCheckNameTests {
+    private final String name;
+    private final boolean expected;
+
+    public AccountCheckNameTests(String name, boolean expected) {
+        this.name = name;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] createDifferentName() {
+        return new Object[][] {
+            { "TW", false},
+            { "3 3", true},
+            { "IVAN IVANOV", true},
+            { "NINETEEN CHARS12345", true},
+            { "TWENTY CHARS12345678", false},
+            { " IVAN IVANOV", false},
+            { "IVAN IVANOV ", false},
+            { "IVAN  IVANOV", false},
+            { "IVANIVANOV", false},
+        };
+    }
+    @Test
+    public void checkNameToEmboss() {
+        boolean actual = new Account(name).checkNameToEmboss();
+        Assert.assertEquals("Method work wrong!", expected, actual);
+    }
+
+}
